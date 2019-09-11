f90da514df4327f7ef49b554258236131514987d
Ahnjunpyo <https://github.com/ahnjp>
2019-09-12 03:09:43 +0900

    백준 단계별문제풀이 for문 1-6

---
+++ 백준 for문 1_6.cpp
@@ -0,0 +1,64 @@
+﻿#include "pch.h"
+#include <stdio.h>
+
+int main(void)
+{
+	//백준 2739번 문제
+	//구구단
+	/*int i, n;
+	scanf_s("%d", &n);
+	for (i = 1; i <= 9; i++)
+		printf("%d * %d = %d\n", n, i, n*i);
+	return 0;*/
+
+	//백준 10950번 문제
+	//A+B - 3
+	/*int a, b, t,i;
+	scanf_s("%d", &t); //테스트할 횟수입력
+
+	for (i = 0; i < t; i++) {
+		scanf_s("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+	return 0;*/
+
+	//백준 8393번 문제
+	//연속되는 수의 합
+	/*int a, i,sum;
+	sum = 0;
+	scanf("%d", &a);
+
+	for (i = 0; i <= a; i++) {
+		sum += i;
+	}
+	printf("%d", sum);
+	return 0;*/
+
+	//백준 15552번 문제
+	//빠른 A+B  //10950문제랑 똑같네
+	/*int a, b, t, i;
+	scanf_s("%d", &t); //테스트할 횟수입력
+
+	for (i = 0; i < t; i++) {
+		scanf_s("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+	return 0;*/
+
+	//백준 2741번 문제
+	//n찍기
+	/*int i, n;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+		printf("%d\n", i);
+	return 0;*/
+
+	//백준 2742번 문제
+	//기찍n
+	/*int i, n;
+	scanf("%d", &n);
+	for (i = n; i >= 1; i--)
+		printf("%d\n", i);
+	return 0;*/
+
+}
